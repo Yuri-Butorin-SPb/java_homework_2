24fb3cc1256cbc9f1b1536d2b7c846c4d512529e
Yuri <butor.yuri@gmail.com>
2023-04-03 15:23:46 +0300

    Add 1 and 2 exercise

--- main2.java
+++ main2.java
@@ -7,7 +7,7 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Main {
+public class main2 {
     public static void main(String[] args) {
         // 1. Дана json строка { {
         // "фамилия":"Иванов","оценка":"5","предмет":"Математика"},
